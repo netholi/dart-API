05668168d31221dc545d48933b2d26d029e2883e
Anand Janardhanan <ajkadakkal@gmail.com>
2025-05-05 19:56:17 +0530

    add dist file and router to list dist by index

--- bin/api2.dart
+++ bin/api2.dart
@@ -1,4 +1,5 @@
 import 'package:api2/api2.dart' as api2;
+import 'dart:convert';
 
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as io;
@@ -6,8 +7,31 @@ import 'package:shelf_router/shelf_router.dart';
 
 void main()async{
 var router=Router();
+
+Response _getDist(){
+return Response.ok('from dist');
+}
+
+
+
 router.get('/',_home);
 router.get('/greet',_greeting);
+
+router.get('/dist/<index>', (Request req, String index){
+final int? parsedIndex= int.tryParse(index);
+if(parsedIndex==null){
+return Response.badRequest(
+body:json.encode(
+{'error':'Invalid Index: $index is not and integer'}
+),
+headers:{'content-type' :'application/json'}
+);
+}
+return _getDist();
+});
+
+
+
 var handler=Pipeline().addMiddleware(logRequests()).addHandler(router);
 var server=await io.serve(handler,"0.0.0.0",8080);
 print("server is live ${server.address.host} : ${server.port}");
---
+++ lib/dist.dart
@@ -0,0 +1,86 @@
+const String dist='''[
+  {
+    "district": "Alappuzha",
+    "headquarters": "Alappuzha",
+    "legislative_constituencies": 9,
+    "parliamentary_constituencies": 1
+  },
+  {
+    "district": "Ernakulam",
+    "headquarters": "Kakkanad",
+    "legislative_constituencies": 14,
+    "parliamentary_constituencies": 2
+  },
+  {
+    "district": "Idukki",
+    "headquarters": "Painavu",
+    "legislative_constituencies": 5,
+    "parliamentary_constituencies": 1
+  },
+  {
+    "district": "Kannur",
+    "headquarters": "Kannur",
+    "legislative_constituencies": 11,
+    "parliamentary_constituencies": 2
+  },
+  {
+    "district": "Kasaragod",
+    "headquarters": "Kasaragod",
+    "legislative_constituencies": 5,
+    "parliamentary_constituencies": 1
+  },
+  {
+    "district": "Kollam",
+    "headquarters": "Kollam",
+    "legislative_constituencies": 11,
+    "parliamentary_constituencies": 1
+  },
+  {
+    "district": "Kottayam",
+    "headquarters": "Kottayam",
+    "legislative_constituencies": 9,
+    "parliamentary_constituencies": 1
+  },
+  {
+    "district": "Kozhikode",
+    "headquarters": "Kozhikode",
+    "legislative_constituencies": 13,
+    "parliamentary_constituencies": 2
+  },
+  {
+    "district": "Malappuram",
+    "headquarters": "Malappuram",
+    "legislative_constituencies": 16,
+    "parliamentary_constituencies": 2
+  },
+  {
+    "district": "Palakkad",
+    "headquarters": "Palakkad",
+    "legislative_constituencies": 12,
+    "parliamentary_constituencies": 2
+  },
+  {
+    "district": "Pathanamthitta",
+    "headquarters": "Pathanamthitta",
+    "legislative_constituencies": 5,
+    "parliamentary_constituencies": 1
+  },
+  {
+    "district": "Thiruvananthapuram",
+    "headquarters": "Thiruvananthapuram",
+    "legislative_constituencies": 14,
+    "parliamentary_constituencies": 2
+  },
+  {
+    "district": "Thrissur",
+    "headquarters": "Thrissur",
+    "legislative_constituencies": 13,
+    "parliamentary_constituencies": 2
+  },
+  {
+    "district": "Wayanad",
+    "headquarters": "Kalpetta",
+    "legislative_constituencies": 3,
+    "parliamentary_constituencies": 1
+  }
+]''';
