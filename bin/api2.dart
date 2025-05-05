4b1ce27cba5d0a945c9ac8d2aaa817f59897379e
Anand Janardhanan <ajkadakkal@gmail.com>
2025-05-05 08:19:23 +0530

    add router service

--- bin/api2.dart
+++ bin/api2.dart
@@ -2,15 +2,25 @@ import 'package:api2/api2.dart' as api2;
 
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as io;
+import 'package:shelf_router/shelf_router.dart';
 
 void main()async{
-var handler=Pipeline().addMiddleware(logRequests()).addHandler(api);
+var router=Router();
+router.get('/',_home);
+router.get('/greet',_greeting);
+var handler=Pipeline().addMiddleware(logRequests()).addHandler(router);
 var server=await io.serve(handler,"0.0.0.0",8080);
 print("server is live ${server.address.host} : ${server.port}");
 }
 
-Response api(Request req){
-return Response.ok("Hello api - simple api");
+Response _home(Request req){
+return Response.ok("Hello api - Home page api");
 }
+
+Response _greeting(Request req){
+return Response.ok('Hello Greetings from api');
+}
+
+
 
 
